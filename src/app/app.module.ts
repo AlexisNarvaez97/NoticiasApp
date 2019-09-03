b815c8cd09957b5bae2923e1cb5b9b788c1a8072
Alexis Narvaez <alexisnarvaez97@hotmail.com>
2019-09-03 14:38:10 -0500

    Action-Sheet and Social Charin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -23,6 +26,8 @@ import { AppComponent } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
+    InAppBrowser,
+    SocialSharing,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Article } from '../../pages/interfaces/interface';
 
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
+import { ActionSheetController } from '@ionic/angular';
+
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+
+
+
 @Component({
   selector: 'app-noticia',
   templateUrl: './noticia.component.html',
@@ -11,8 +19,53 @@ export class NoticiaComponent implements OnInit {
   @Input() noticia: Article;
   @Input() indice: number;
 
-  constructor() { }
+  constructor(private iab: InAppBrowser, public actionSheetController: ActionSheetController, private socialSharing: SocialSharing) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
+
+  abrirNoticia() {
+    console.log(this.noticia.url);
+
+    const browser = this.iab.create(this.noticia.url, '_system');
+  }
+
+  async lanzarMenu() {
+
+    const actionSheet = await this.actionSheetController.create({
+      buttons: [
+        {
+          text: 'Compartir',
+          icon: 'share',
+          cssClass: 'action-dark',
+          handler: () => {
+            console.log('Share clicked');
+            this.socialSharing.share(
+              this.noticia.title,
+              this.noticia.source.name,
+              '',
+              this.noticia.url
+            )
+          }
+        },
+        {
+          text: 'Favorite',
+          icon: 'heart',
+          cssClass: 'action-dark',
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        },
+        {
+          text: 'Cancelar',
+          icon: 'close',
+          cssClass: 'action-dark',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }]
+    });
+    await actionSheet.present();
+  }
 
 }
